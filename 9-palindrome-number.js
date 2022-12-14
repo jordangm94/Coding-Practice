84fb2bffa19269b6f9b2dc3d6b18fb5ba0ea5a83
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-12-14 12:28:03 +0000

    Solved problem using a combo of string and array prototype methods

--- 9-palindrome-number.js
+++ 9-palindrome-number.js
@@ -1,5 +1,15 @@
 // Given an integer x, return true if x is a palindrome, and false otherwise.
 
-var isPalindrome = function(x) {
-  
+var isPalindrome = function(num) {
+  //Convert num to string, split numbers into array, reverse array, rejoint he numberss
+  let numReversed = num.toString().split("").reverse().join("");
+
+  //Use == instead of === so that string to number comparison can be made
+  if (num == numReversed) {
+    return true
+  } else {
+    return false
+  }
 }
+
+console.log(isPalindrome(125));
