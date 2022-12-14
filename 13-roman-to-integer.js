3f6619080f8e8744cf47ef4c76ca8799ae5f031d
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-12-14 12:42:50 +0000

    Created file for roman to integer question

---
+++ 13-roman-to-integer.js
@@ -0,0 +1,16 @@
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+/*
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+*/
+
+//Given a roman numeral, convert it to an integer.
+
+var romanToInt = function(s) {
+  
+}
